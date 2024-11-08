7cd06ad40a418f86083df188c9ea827249706185
Jose Alvarez <jlam97@hotmail.es>
2024-11-07 20:26:42 -0600

    Implementacion de metodos en package 'service' para eliminar, guardar y porId

--- src/main/java/org/servlet/webapp/cursos/service/CursoService.java
+++ src/main/java/org/servlet/webapp/cursos/service/CursoService.java
@@ -2,9 +2,18 @@ package org.servlet.webapp.cursos.service;
 
 import org.servlet.webapp.cursos.model.Curso;
 import java.util.List;
+import java.util.Optional;
 
 public interface CursoService {
+
     List<Curso> listar();
+
     List<Curso> porNombre(String nombre);
+
+    Optional<Curso> porId(Long id);
+
+    void guardar(Curso curso);
+
+    void eliminar(Long id);
 }
 
